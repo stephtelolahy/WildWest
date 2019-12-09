d8dc75c73130d0f94682ad626be27403ae897cf7
Telolahy <stephano.telolahy@gmail.com>
2019-12-09 21:04:11 +0100

    Initial Commit

---
+++ WildWest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WildWest
+//
+//  Created by Hugues Stéphano TELOLAHY on 12/9/19.
+//  Copyright © 2019 creativeGames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
