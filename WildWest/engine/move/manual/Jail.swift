7195c9cd8e330e93a4b5e3d9ca4dc658364643bd
Telolahy <stephano.telolahy@gmail.com>
2020-03-21 11:12:53 +0100

    enginev3 +jail

--- WildWest/engine/move/manual/Jail.swift
+++ WildWest/engine/move/manual/Jail.swift
@@ -6,24 +6,8 @@
 //  Copyright © 2019 creativeGames. All rights reserved.
 //
 
-struct Jail: PlayCardAgainstOnePlayerActionProtocol, Equatable {
-    let actorId: String
-    let cardId: String
-    let targetId: String
-    let autoPlay = false
-    
-    func execute(in state: GameStateProtocol) -> [GameUpdateProtocol] {
-        [GameUpdate.playerPutInPlayOfOther(actorId, targetId, cardId)]
-    }
-    
-    var description: String {
-        "\(actorId) put \(targetId) in jail"
-    }
-}
-
-struct JailRule: RuleProtocol {
-    
-    func match(with state: GameStateProtocol) -> [ActionProtocol]? {
+class JailMatcher: ValidMoveMatcherProtocol {
+    func validMoves(matching state: GameStateProtocol) -> [GameMove]? {
         guard state.challenge == nil,
             let actor = state.players.first(where: { $0.identifier == state.turn }),
             let cards = actor.handCards(named: .jail) else {
@@ -45,7 +29,27 @@ struct JailRule: RuleProtocol {
         }
         
         return cards.map { card in
-            otherPlayers.map { Jail(actorId: actor.identifier, cardId: card.identifier, targetId: $0.identifier) }
+            otherPlayers.map {
+                GameMove(name: .play,
+                         actorId: actor.identifier,
+                         cardId: card.identifier,
+                         cardName: card.name,
+                         targetId: $0.identifier)
+            }
         }.flatMap { $0 }
+    }
+}
+
+class JailExecutor: MoveExecutorProtocol {
+    func execute(_ move: GameMove, in state: GameStateProtocol) -> [GameUpdate]? {
+        guard case .play = move.name,
+            let actorId = move.actorId,
+            let cardId = move.cardId,
+            case .jail = move.cardName,
+            let targetId = move.targetId else {
+                return nil
+        }
+        
+        return [.playerPutInPlayOfOther(actorId, targetId, cardId)]
     }
 }
--- WildWestTests/action/JailTests.swift
+++ WildWestTests/action/JailTests.swift
@@ -33,83 +33,10 @@ class JailTests: XCTestCase {
         XCTAssertEqual(sut.description, "p1 put p2 in jail")
     }
     
-    func test_PutCardInPlayOfTargetPlayer_IfPlayingJail() {
-        // Given
-        let mockState = MockGameStateProtocol()
-        let sut = Jail(actorId: "p1", cardId: "c1", targetId: "p2")
-        
-        // When
-        let updates = sut.execute(move, in: mockState)
-        
-        // Assert
-        XCTAssertEqual(updates as? [GameUpdate], [.playerPutInPlayOfOther("p1", "p2", "c1")])
-    }
+    
 }
 
 class JailRuleTests: XCTestCase {
     
-    func test_CanPlayJail_IfHoldingCardAndAgainsNonSheriffPlayer() {
-        // Given
-        let mockPlayer1 = MockPlayerProtocol()
-            .identified(by: "p1")
-            .holding(MockCardProtocol().identified(by: "c1").named(.jail))
-        let mockPlayer2 = MockPlayerProtocol()
-            .identified(by: "p2")
-            .role(is: .outlaw)
-            .noCardsInPlay()
-        let mockState = MockGameStateProtocol()
-            .challenge(is: nil)
-            .currentTurn(is: "p1")
-            .players(are: mockPlayer1, mockPlayer2)
-        let sut = JailRule()
-        
-        // When
-        let moves = sut.validMoves(matching: mockState)
-        
-        // Assert
-        XCTAssertEqual(actions as? [Jail], [Jail(actorId: "p1", cardId: "c1", targetId: "p2")])
-    }
     
-    func test_CannotPlayJail_IfTargetPlayerIsSheriff() {
-        // Given
-        let mockPlayer1 = MockPlayerProtocol()
-            .identified(by: "p1")
-            .holding(MockCardProtocol().identified(by: "c1").named(.jail))
-        let mockPlayer2 = MockPlayerProtocol()
-            .identified(by: "p2")
-            .role(is: .sheriff)
-        let mockState = MockGameStateProtocol()
-            .challenge(is: nil)
-            .currentTurn(is: "p1")
-            .players(are: mockPlayer1, mockPlayer2)
-        let sut = JailRule()
-        
-        // When
-        let moves = sut.validMoves(matching: mockState)
-        
-        // Assert
-        XCTAssertNil(actions)
-    }
-    
-    func test_CannotPlayJail_IfTargetPlayerIsAlreadyInJail() {
-        // Given
-        let mockPlayer1 = MockPlayerProtocol()
-            .identified(by: "p1")
-            .holding(MockCardProtocol().identified(by: "c1").named(.jail))
-        let mockPlayer2 = MockPlayerProtocol()
-            .identified(by: "p2")
-            .role(is: .deputy)
-            .playing(MockCardProtocol().named(.jail))
-        let mockState = MockGameStateProtocol()
-            .challenge(is: nil)
-            .currentTurn(is: "p1")
-            .players(are: mockPlayer1, mockPlayer2)
-        let sut = JailRule()
-        
-        // When
-        let moves = sut.validMoves(matching: mockState)
-        
-        // Assert
-        XCTAssertNil(actions)
-    }
 }
