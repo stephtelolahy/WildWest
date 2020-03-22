6fe0fe43215d2cbb1ef6de086da82c30211a39d8
Telolahy <stephano.telolahy@gmail.com>
2020-03-22 11:32:12 +0100

    fix jail matcher

--- WildWest/ui/menu/MenuViewController.swift
+++ WildWest/ui/menu/MenuViewController.swift
@@ -59,8 +59,8 @@ class MenuViewController: UIViewController {
                                 moveExecutors: config.moveExectors,
                                 updateExecutors: config.updateExecutors)
         
-        let controlledPlayerId: String? = nil
-        //        let controlledPlayerId = state.players.first(where: { $0.role == .sheriff })?.identifier
+        //        let controlledPlayerId: String? = nil
+        let controlledPlayerId = state.players.first(where: { $0.role == .sheriff })?.identifier
         
         let aiPlayers = database.state.players.filter { $0.identifier != controlledPlayerId }
         
