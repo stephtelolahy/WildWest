0c244e97c9066046f7892c372d34b53f2d33763c
Telolahy <stephano.telolahy@gmail.com>
2020-04-22 18:52:36 +0200

    configure database

---
+++ WildWest/engine/database/FirebaseDatabase.swift
@@ -0,0 +1,12 @@
+//
+//  FirebaseDatabase.swift
+//  WildWest
+//
+//  Created by Hugues Stephano Telolahy on 22/04/2020.
+//  Copyright © 2020 creativeGames. All rights reserved.
+//
+
+import Firebase
+
+class FirebaseDatabase {
+}
