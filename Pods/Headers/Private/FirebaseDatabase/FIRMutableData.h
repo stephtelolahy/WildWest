86ebaf4800af099af7e0fc0b242c9c4a742674e7
Telolahy <stephano.telolahy@gmail.com>
2020-08-03 09:06:25 +0200

    .gitignore is now working

--- Pods/Headers/Private/FirebaseDatabase/FIRMutableData.h
+++
@@ -1,1 +0,0 @@
-../../../FirebaseDatabase/FirebaseDatabase/Sources/Public/FIRMutableData.h
--- Pods/Target Support Files/FirebaseDatabase-library/FirebaseDatabase-library-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FirebaseDatabase_library : NSObject
-@end
-@implementation PodsDummy_FirebaseDatabase_library
-@end
